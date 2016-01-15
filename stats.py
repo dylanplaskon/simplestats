1414c69cbf6722540b4c692856e4667f72cf7547
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 13:28:00 -0600

    Stats 3.0 added test function

--- stats.py
+++ stats.py
@@ -5,6 +5,9 @@ def mean(vals):
 	total = sum(vals)
 	length = len(vals)
 	return total/length
+
+def test_mean():	
+	assert mean([2,4]) == 3.0
+test_mean()
 	
-#print(mean([2,4]))
-print(mean("hello"))
+#print(mean("hello"))
