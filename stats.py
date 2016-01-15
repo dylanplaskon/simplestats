8bbc8d860cb62c5d9313df48f79a934d042a6ce0
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 14:24:43 -0600

    added support for strings

--- stats.py
+++ stats.py
@@ -1,20 +1,17 @@
 def mean(vals):
 	"""Calculate the arithmetic mean of a list of numbers in vals"""
 	assert type(vals) is list, 'input must be list'
-	if vals == []:
+	ivals = []
+	for n in vals:
+		ivals.append(float(n))
+		
+	if ivals == []:
 		return 0
 	else:
-		total = sum(vals)
-		length = len(vals)
+		total = sum(ivals)
+		length = len(ivals)
 		return total/length
 
-def test_mean():	
-	assert mean([2,4]) == 3.0
-test_mean()
-	
-def test_empty_list():
-	assert mean([]) == 0.0, 'need to handle empty lists'
-test_empty_list()
-
+	print(ivals)
 	
 #print(mean("hello"))
--- teststats.py
+++ teststats.py
@@ -12,3 +12,7 @@ def test_empty_list():
 def test_float():
 	assert_almost_equals(mean([0.5, 0.5, 1]), 0.66666666), 'float is bad'
 #test_float()
+
+def test_str_list_mean():
+	assert_equal(mean(['1', '2', '3']), 2.0), 'strings are bad'
+#test_str_list_mean()
