46e6faf215875c36e8928bc4dcbdec2cb79e0dcc
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 13:59:35 -0600

    using nosetests

--- teststats.py
+++ teststats.py
@@ -1,13 +1,14 @@
 from stats import mean
+from nose.tools import assert_equal
 
 def test_mean():	
-	assert mean([2,4]) == 3.0
-test_mean()
+	assert_equal(mean([2, 4]), 3.0)
+#test_mean()
 	
 def test_empty_list():
-	assert mean([]) == 0.0, 'need to handle empty lists'
-test_empty_list()
+	assert_equal(mean([]), 0.0), 'need to handle empty lists'
+#test_empty_list()
 
 def test_float():
 	assert mean([0.2, 0.4]) == 0.3, 'float is bad'
-test_float()
+#test_float()
