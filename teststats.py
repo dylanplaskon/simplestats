5570e7d9155194b28ab4b3099d71e630ad02d306
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 14:05:39 -0600

    using nose assert_equal and assert_amost_equal

--- teststats.py
+++ teststats.py
@@ -1,5 +1,5 @@
 from stats import mean
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():	
 	assert_equal(mean([2, 4]), 3.0)
@@ -10,5 +10,5 @@ def test_empty_list():
 #test_empty_list()
 
 def test_float():
-	assert mean([0.2, 0.4]) == 0.3, 'float is bad'
+	assert_almost_equals(mean([0.5, 0.5, 1]), 0.66666666), 'float is bad'
 #test_float()
