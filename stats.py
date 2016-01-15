906c5f347c8635cc9a9bb235798023d1010a53fa
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 13:36:53 -0600

    Stats 4.0 added [] == 0

--- stats.py
+++ stats.py
@@ -1,13 +1,20 @@
 def mean(vals):
 	"""Calculate the arithmetic mean of a list of numbers in vals"""
 	assert type(vals) is list, 'input must be list'
-	
-	total = sum(vals)
-	length = len(vals)
-	return total/length
+	if vals == []:
+		return 0
+	else:
+		total = sum(vals)
+		length = len(vals)
+		return total/length
 
 def test_mean():	
 	assert mean([2,4]) == 3.0
 test_mean()
+	
+def test_empty_list():
+	assert mean([]) == 0.0, 'need to handle empty lists'
+test_empty_list()
+
 	
 #print(mean("hello"))
