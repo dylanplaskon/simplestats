6a3dc32643d74e264b29a482bde18b25e370da67
Dylan Plaskon <plaskon@wisc.edu>
2016-01-15 13:23:27 -0600

    Stats v2.0 added list assertion

--- stats.py
+++ stats.py
@@ -1,7 +1,10 @@
 def mean(vals):
 	"""Calculate the arithmetic mean of a list of numbers in vals"""
+	assert type(vals) is list, 'input must be list'
+	
 	total = sum(vals)
 	length = len(vals)
 	return total/length
 	
-print(mean([2,4]))
+#print(mean([2,4]))
+print(mean("hello"))
